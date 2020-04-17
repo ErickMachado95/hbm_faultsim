15ddd0569a2fda6755ca846bd27c801eb8d42b3f
ErickMachado95 <machado.m.erick@gmail.com>
2020-04-16 23:04:18 -0500

    Changes for compiling data

--- scripts/graph.py
+++ scripts/graph.py
@@ -15,7 +15,6 @@ if(len(sys.argv) == 3):
     for i in range(0,len(df1.columns)):
         values.append(df1.iloc[:,i].sum())
     faults = ['t_1bit','t_1word','t_1col','t_1row','t_1bank','t_nbank','t_nrank','p_1bit','p_1word','p_1col','p_1row','p_1bank','p_nbank','p_nrank']
-   
     y_pos = np.arange(len(faults))
     plt.bar(y_pos,values,align='center')
     plt.xticks(y_pos,faults,rotation='vertical')
@@ -24,5 +23,6 @@ if(len(sys.argv) == 3):
     plt.title(graph_name)
     plt.tight_layout()
     plt.savefig(graph_name + ".png")
+
 else:
     print("Wrong number of arguments")
