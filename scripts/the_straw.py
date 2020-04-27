ae323a17779f6247c778f38ddc2d9a8196d01b92
ErickMachado95 <machado.m.erick@gmail.com>
2020-04-27 12:29:04 -0500

    Updated scripts

---
+++ scripts/the_straw.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+
+def get_the_straws(csv_name):
+    df = pd.read_csv("synop.csv");
+
+    s_out = open(csv_name+"_s.csv","w")
+
+    s_out.write("dram_module,sim_num,sim_stat,trans_1bit,trans_1word,trans_1col,trans_1row,trans_1bank,trans_nbank,trans_nrank,"
+    +"perm_1bit,perm_1word,perm_1col,perm_1row,perm_1bank,perm_nbank,perm_nrank,the_straw\n")
+
+    s_out.close()
+
+    df = df.loc[df['the_straw'] > 0]
+
+
+    print(df)
+    df.to_csv(csv_name+"_s.csv",mode="a",index=False,header=False)
+
+
+
+
